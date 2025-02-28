c9335c84a8dd252c9c07043591521c7184a32a51
Makinana267 <iviwe.makinana@sambeconsulting.com>
2025-02-28 11:10:49 +0200

    Delete select.sql

--- select.sql
+++
@@ -1,9 +0,0 @@
-USE StudentRecords;
-GO
-INSERT INTO Students (FirstName, LastName, Age)
-VALUES 
-    ('John', 'Doe', 20),
-    ('Jane', 'Smith', 22),
-    ('Mike', 'Johnson', 19);
-GO
-SELECT * FROM Students;
