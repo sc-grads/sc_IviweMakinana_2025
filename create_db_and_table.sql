f391d8e12da208fba2dd3e06569ce8de2e432539
Makinana267 <iviwe.makinana@sambeconsulting.com>
2025-03-26 09:25:56 +0200

    Create create_db_and_table.sql

---
+++ create_db_and_table.sql
@@ -0,0 +1,21 @@
+-- Create the database if it doesn't exist
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'AutoTest')
+BEGIN
+    CREATE DATABASE AutoTest;
+END
+GO
+
+-- Switch to the AutoTest database
+USE AutoTest;
+GO
+
+-- Create the user table if it doesn't exist
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'user')
+BEGIN
+    CREATE TABLE [user] (
+        Name NVARCHAR(50),
+        Surname NVARCHAR(50),
+        Email NVARCHAR(100)
+    );
+END
+GO
